ae2ceea403e0552920e1d28c1f80ee83935f963f
franco-giglio <giglio4605@gmail.com>
2023-10-03 19:47:02 -0300

    login

--- ProyectoTAI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ProyectoTAI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -129,7 +129,7 @@ namespace ProyectoTAI.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Acceso invalido.");
                     return Page();
                 }
             }
